42cce0fa80b1fe42a663400a4ae3fa23b36c04f3
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-07-06 17:04:31 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+        <section className="section--img--uploader">
+            <h1>Hola wachin</h1>
+            <h2>portate bien</h2>
+        </section>
+    )
+}
