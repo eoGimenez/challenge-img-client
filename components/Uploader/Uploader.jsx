d628dfbeffaa47eece689169608a23feecb5f957
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-07-09 19:27:43 +0200

    maquetando y armando estilos de la primera pimera pantalla, sigue agregar loading

--- components/Uploader/Uploader.jsx
+++ components/Uploader/Uploader.jsx
@@ -1,16 +1,16 @@
-import { useFile } from '../../hooks/useFile';
 import UploadButton from '../UploadButton/UploadButton';
 import './Uploader.css';
 
 export default function Uploader() {
-	// const { handleDrag, handleDrop } = useFile();
 
 	return (
 		<section className='section--uploader--img'>
 			<div className='uploader--container'>
-				<div className='img--container'>aca va el logo de la imagen</div>
-
-				<UploadButton />
+				<h1>Upload your image</h1>
+				<p>File should be jpg, jpeg, png or svg</p>
+				<div className='img--container'>
+					<UploadButton />
+				</div>
 			</div>
 		</section>
 	);
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
+import './App.css'
 import Uploader from '../components/Uploader/Uploader';
 
 export default function App() {
-	
 	return (
 		<section className='section--img--uploader'>
-			<h1>Hola wachin</h1>
 			<Uploader />
 		</section>
 	);
