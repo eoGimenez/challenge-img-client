c32b4de8caed573b7e0b5047de13287c4a10341e
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-07-09 23:53:46 +0200

    funcionalidades ok, terminar de ajustar estilos a mis gustos

--- components/Successfull/Successfull.jsx
+++ components/Successfull/Successfull.jsx
@@ -1,14 +1,21 @@
 import './Successfull.css';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 export default function Successfull({ image }) {
 	return (
 		<div className='successfull--container'>
+			<span class='material-symbols-outlined'>check_circle</span>
 			<h3>Upload successfully</h3>
 			<div className='img--container'>
 				{' '}
 				<img src={image.path} alt='Image uploaded' />
 			</div>
-			<p>{image.path} Ruta para copiar</p>
+			<div className='copy--path--container'>
+				<p className='path--parraf'>{image.path}</p>
+				<CopyToClipboard text={image.path}>
+					<button className='btn--copy--path'>Copy Url</button>
+				</CopyToClipboard>
+			</div>
 		</div>
 	);
 }
